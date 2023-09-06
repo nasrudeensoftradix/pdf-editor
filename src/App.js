7793f74e094457359ff748a19335ddc03c0c436c
nasrudeen ansari <nasrudeen@softradix.in>
2023-09-06 16:17:55 +0530

    Updated build

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import PdfViewerComponent from "./components/PdfViewerComponent";
 import "./App.css";
 
 function App() {
-  const [document, setDocument] = useState("document.pdf");
+  const [document, setDocument] = useState("document2.pdf");
 
   const handleOpen = () => setDocument("another-example.pdf");
 
   return (
     <div className="App">
       <button className="App-button" onClick={handleOpen}>
-        Open another document
+        Softradix PDF editor
       </button>
       <div className="App-viewer">
         <PdfViewerComponent document={document} />
--- src/components/PdfViewerComponent.js
+++ src/components/PdfViewerComponent.js
@@ -1,26 +1,38 @@
 import { useEffect, useRef } from "react";
-
+import PSPDFKit from "pspdfkit";
 export default function PdfViewerComponent(props) {
   const containerRef = useRef(null);
+  let instance = null;
 
   useEffect(() => {
     const container = containerRef.current;
-    let PSPDFKit;
+    // let PSPDFKit;
 
     (async function () {
-      PSPDFKit = await import("pspdfkit");
+      // PSPDFKit = await import("pspdfkit");
       await PSPDFKit.load({
+        toolbarItems: [
+          ...PSPDFKit.defaultToolbarItems,
+          { type: "content-editor" },
+        ],
         // Container where PSPDFKit should be mounted.
         container,
         // The document to open.
         document: props.document,
         // Use the public directory URL as a base URL. PSPDFKit will download its library assets from here.
         baseUrl: `${window.location.protocol}//${window.location.host}/${process.env.PUBLIC_URL}`,
+        // Enable the editor option.
+      }).then((pdfInstance) => {
+        instance = pdfInstance;
       });
     })();
 
     return () => PSPDFKit && PSPDFKit.unload(container);
   }, [props.document]);
 
-  return <div ref={containerRef} style={{ width: "100%", height: "100vh" }} />;
+  return (
+    <>
+      <div ref={containerRef} style={{ width: "100%", height: "100vh" }} />;
+    </>
+  );
 }
